16345f505b61a7ebcc34cc6cc44865a315d012a9
calocan <andy@calthorpeanalytics.com>
2016-08-02 22:20:16 +0200

    Added a bunch of css stuff and created a production webpack.dev.config

---
+++ webpack.prod.config.js
@@ -0,0 +1,76 @@
+/**
+ * Created by Andy Likuski on 2016.08.02
+ * Copyright (c) 2016 Andy Likuski
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const APP = __dirname + '/app';
+const BUILD = __dirname + '/build';
+const STYLE = __dirname + '/app/style.css';
+const PACKAGE = Object.keys(
+    require('./package.json').dependencies
+);
+const TEMPLATE = __dirname + '/app/templates/index_default.html'
+const PostcssImport = require('postcss-easy-import');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: {
+        app: APP,
+        style: STYLE,
+        vendor: PACKAGE
+    }, resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    output: {
+        path: BUILD,
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[chunkhash].js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loaders: ['babel?cacheDirectory'],
+                include: APP
+            },
+            // Extract CSS during build
+            {
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract('style', 'css!postcss'),
+                include: APP
+            }]
+    },
+    postcss: function () {
+        return [precss, autoprefixer];
+    },
+    plugins: [
+        new CleanPlugin([BUILD]),
+        new HtmlWebpackPlugin({template: TEMPLATE, inject: 'body'}),
+        new ExtractTextPlugin('[name].[chunkhash].css'), new webpack.optimize.DedupePlugin(),
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
+        }),
+
+    ]
+};
