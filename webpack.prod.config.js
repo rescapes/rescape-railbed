49729dfc3c6876ffcad323f76d26f5c7bcf53968
calocan <andy@calthorpeanalytics.com>
2016-10-03 18:42:38 -0700

    Got production build working

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -10,35 +10,46 @@
  */
 
 const webpack = require('webpack');
+
+// File ops
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// Folder ops
+const CleanPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const PUBLIC = __dirname + '/app/public';
-const APP = __dirname + '/app';
-const BUILD = __dirname + '/build';
-const STYLE = __dirname + '/app/style.css';
+const path = require('path');
+
+// PostCSS support
+const postcssImport = require('postcss-easy-import');
+const precss = require('precss');
+const autoprefixer = require('autoprefixer');
+
+// Constants
+const APP = path.join(__dirname, 'app');
+const BUILD = path.join(__dirname, 'build');
+const STYLE = path.join(__dirname, 'app/style.css');
+const PUBLIC = path.join(__dirname, 'app/public');
+const TEMPLATE = path.join(__dirname, 'app/templates/index_default.html');
+
 const PACKAGE = Object.keys(
     require('./package.json').dependencies
 );
-const TEMPLATE = __dirname + '/app/templates/index_default.html'
-const PostcssImport = require('postcss-easy-import');
-const precss = require('precss');
-const autoprefixer = require('autoprefixer');
 
 module.exports = {
     entry: {
         app: APP,
-        style: STYLE,
-        vendor: PACKAGE
+        style: STYLE
+        //vendor: PACKAGE
     },
     resolve: {
-        extensions: ['', '.js']
+        //extensions: ['', '.js', '.jsx']
     },
     output: {
         path: BUILD,
         filename: '[name].[chunkhash].js',
-        chunkFilename: '[chunkhash].js'
+        chunkFilename: '[chunkhash].js',
+        publicPath: '/'
     },
     module: {
         loaders: [
@@ -52,36 +63,87 @@ module.exports = {
                 test: /\.css$/,
                 loader: ExtractTextPlugin.extract('style', 'css!postcss'),
                 include: APP
-            }]
-    },
-    postcss: function () {
-        return [
-            precss,
-            autoprefixer({ browsers: ['last 2 versions'] })
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                loader: 'file-loader?name=images/[name].[ext]',
+                include: APP
+            },
+            {
+                test: /\.(webm)$/,
+                loader: 'file-loader?name=videos/[name].[ext]',
+                include: APP
+            },
+            // Process JSON data fixtures
+            {
+                test: /\.json$/,
+                loader: 'json',
+                include: APP
+            }
         ]
     },
+    postcss: function processPostcss(webpack) {  // eslint-disable-line no-shadow
+        return [
+            postcssImport({
+                addDependencyTo: webpack
+            }),
+            precss,
+            autoprefixer({ browsers: ['last 2 versions'] })
+        ];
+    },
+    // Remove comment if you require sourcemaps for your production code
+    // devtool: 'cheap-module-source-map',
     plugins: [
+        // Required to inject NODE_ENV within React app.
+        // Reduntant package.json script entry does not do that, but required for .babelrc
+        // Optimizes React for use in production mode
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production') // eslint-disable-line quote-props
+            }
+        }),
+        // Clean build directory
         new CleanPlugin([BUILD]),
+        new CopyWebpackPlugin([
+                { from: PUBLIC, to: BUILD }
+            ],
+            {
+                ignore: [
+                    // Doesn't copy Mac storage system files
+                    '.DS_Store'
+                ]
+            }
+        ),
+        // Auto generate index.html
         new HtmlWebpackPlugin({
             template: TEMPLATE,
-            inject: 'body'
+            // JS placed at the bottom of the body element
+            inject: 'body',
+            // Use html-minifier
+            minify: {
+                collapseWhitespace: true
+            }
         }),
-        new ExtractTextPlugin('[name].[chunkhash].css'), new webpack.optimize.DedupePlugin(),
+
+        // Extract CSS to a separate file
+        new ExtractTextPlugin('[name].[chunkhash].css'),
+
+        // Remove comment to dedupe duplicating dependencies for larger projects
+        // new webpack.optimize.DedupePlugin(),
+
+        // Separate vendor and manifest files
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor', 'manifest']
         }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                'NODE_ENV': JSON.stringify('production')
-            }
-        }),
+
+        // Minify JavaScript
         new webpack.optimize.UglifyJsPlugin({
             compress: {
                 warnings: false
             }
-        }),
-        new CopyWebpackPlugin([
-            { from: PUBLIC, to: BUILD }
-        ])
+        })
+        // Error: vendor.450f275….js:16 Uncaught TypeError: Cannot read property 'shape' of undefined
+        // Error: manifest.798b47f….js:1 Uncaught TypeError: Cannot read property 'call' of undefined
+        // new webpack.optimize.OccurrenceOrderPlugin()
     ]
 };
