cf896f6e46989663bfb388f1e28fb49cacfcd507
calocan <andy@calthorpeanalytics.com>
2016-10-31 16:07:57 -0700

    Added overlay document download button

--- app/components/OverlayDocument.js
+++ app/components/OverlayDocument.js
@@ -36,7 +36,7 @@ class OverlayDocument extends Component {
      * Downloads the underlying document
      */
     handleDownload() {
-        forceDownload(this.documentDiv, this.props.document.get('url'))
+        forceDownload(document, this.props.document.get('url'))
     }
 
     /***
